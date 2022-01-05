6cf2d94f052faa1da19120fefc22593a1adaef22
awtlkrakow <94865935+awtlkrakow@users.noreply.github.com>
2022-01-05 15:38:09 +0100

    Change message.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.json({ message: "es6 modules are awesome but tiring sometimes" });
+    res.json({ message: "Dzień dobry! Trzeba było dodać \"--preset env\" do babela ale działa :)" });
 });
 
 app.listen(port, () => console.log("server listening on port", port));
